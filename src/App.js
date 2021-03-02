c0b50fc4995c14a5dcf1e89ee95d2b61114841b2
RaphielHS <70839561+RaphielHS@users.noreply.github.com>
2021-03-02 08:52:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import Services from './components/pages/Services';
 import Projects from './components/pages/Projects';
+import PageNotFound from './components/pages/404.js'
 // import SignUp from './components/pages/SignUp';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/projects' component={Projects} />
           {/* <Route path='/sign-up' component={SignUp} /> */}
+          <Route path="" component={ PageNotFound }/>
         </Switch>
       </Router>
     </>
