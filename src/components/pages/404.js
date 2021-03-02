0baaea0f5c723f8dfd1ee4c973f9a768f05b4242
RaphielHS <70839561+RaphielHS@users.noreply.github.com>
2021-03-02 08:52:13 +0700

    Create 404.js

---
+++ src/components/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../HeroSection.css';
+
+
+function PageNotFound() {
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+        </div>)
+}
+export default PageNotFound;
